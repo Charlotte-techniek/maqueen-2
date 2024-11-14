efb8b5d951ae715325a9e0371c84a552c77c8588
Charlotte-techniek <charlotte.malfliet99@gmail.com>
2024-11-14 09:54:54 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -10,8 +10,10 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 44)
     } else if (IR.IR_read() == links) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 44)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 44)
     } else if (IR.IR_read() == rechts) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 44)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 44)
     } else if (IR.IR_read() == stop) {
         maqueen.motorStop(maqueen.Motors.All)
     }
